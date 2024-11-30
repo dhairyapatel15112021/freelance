cb6d0c3ab0dbee8e8523b69e3f25101ad6239c5b
dhairyapatel15112021 <pateldhairya0210@gmail.com>
2024-11-30 19:53:17 +0530

    testimonialContent

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,14 +1,43 @@
+import { useRef } from 'react';
+import { motion, useTransform, useScroll } from "framer-motion";
 import { TestimonialsContent } from './TestimonialsContent'
 
 export const Testimonials = () => {
+  const targetRef = useRef<HTMLDivElement | null>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: targetRef
+  });
+
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-95%"]);
+
+  const data = [ { text : "The décor was breathtaking, and the special effects left everyone speechless! Our first dance felt like a fairytale thanks to their choreography." , name : "Rahul Singh"},
+    {text : "They transformed our event into something magical. Couldn’t have asked for more!" , name : "Simran & Kunal"},
+    {text : "D Effects made our wedding truly magical! The décor was stunning, and the choreography for our first dance was so much fun to learn. Our guests were amazed!”" , name :"Rohan & Meera"},
+    {text : "D Effects made our corporate event feel like a grand celebration. Professional and creative in every way!" , name : "Sunil K." , },
+    {text : "The décor was breathtaking, and the special effects added that extra spark we were hoping for. They truly went above and beyond!" , name : "Shruti & Abhishek"}
+  ]
+
   return (
-    <div className='mt-16 flex flex-col justify-center items-center bg-red-500'>
-        <div className='font-cormorant font-semibold text-4xl md:text-5xl'>
-            Hear From Our Clients
+    <div className='left-0 mt-16 flex flex-col justify-center items-center'>
+      <div className='sticky top-[64px] font-cormorant font-semibold text-4xl md:text-5xl'>
+        Hear From Our Clients
+        {/* https://www.youtube.com/watch?v=4ehYkfh7P-I */}
+      </div>
+      <div ref={targetRef} className='relative h-[1000px] md:h-[900px] w-[95vw]'>
+        <div className='sticky top-[176px] flex items-center mt-16 overflow-hidden'>
+          {/* overflow-hidden for above div */}
+          <motion.div style={{x}} className='flex gap-4'>
+            {
+              data.map((item,index)=>{
+                return(
+                  <TestimonialsContent key={index} text={item.text} name={item.name}/>
+                )
+              })
+            }
+          </motion.div>
         </div>
-        <div>
-            <TestimonialsContent/>
-        </div>
+      </div>
     </div>
   )
 }
--- src/components/TestimonialsContent.tsx
+++ src/components/TestimonialsContent.tsx
@@ -1,5 +1,16 @@
-export const TestimonialsContent = () => {
+import testimonialIcon from "../assets/testimonial/testimonialIcon.svg"
+interface data {
+  text: string,
+  name: string
+}
+export const TestimonialsContent = (inputData: data) => {
   return (
-    <div>TestimonialsContent</div>
+    <div className="h-[250px] w-[400px] relative top-0 font-poppins text-sm md:text-base font-extralight md:font-light">
+      <img src={testimonialIcon} alt="testimonialIcon" className="absolute top-0 left-0 ml-6 z-10" />
+      <div className="absolute top-5 h-[90%] w-full bg-[#282828] px-6 pt-12 pb-6 rounded-md flex flex-col justify-between">
+        <div>{inputData.text}</div>
+        <div> - {inputData.name}</div>
+      </div>
+    </div>
   )
 }
